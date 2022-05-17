2e2c7268b5fa343829240e659500434e2c94a22a
Toni Estabillo <tonimaristelacarpio@gmai.com>
2022-05-17 16:56:58 +0800

    all data files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { EmployeeCard } from './components/EmployeeCard';
 
 function App() {
+
+  const employeeData = [{
+    name: "Danioa Mendez",
+    jobTitle: "HR Professional",
+    department: 'Human Resources'
+  },
+  {
+    name: "Toni Carpio",
+    jobTitle: "Software Engineer",
+    department: 'Engineering'
+  },
+  {
+    name: "Ethyl",
+    jobTitle: "Software Engineer",
+    department: 'Engineering'
+  }
+]
+
+const selection = 2;
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+          Ethyl's Employee Database
+        </h1>
+        <div className="card-container">
+            {
+              employeeData.map((employee)=>{
+                return(
+                  <EmployeeCard
+                    name={employee.name}
+                    jobTitle={employee.jobTitle}
+                    department={employee.department}
+                  />
+                )})
+            }
+        </div>
+        {/* 
+           LOGIC evaluation
+           AND   && 
+
+           T && T = T
+           T && F = F
+           F && T = F
+           F && F = F
+        
+        */}
+        {
+            selection === 1 && <div className="selection"> THIS IS FOR SELECTION 1 - Danica ONLY </div>
+        }
+        {
+            selection === 2 && <div className="selection"> THIS IS FOR SELECTION 2 - Toni ONLY </div>
+        }
+        {
+            selection === 3 && <div className="selection"> THIS IS FOR SELECTION 3 - Ethyl ONLY </div>
+        }
     </div>
   );
 }
---
+++ src/components/EmployeeCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './EmployeeCard.css'
+
+const EmployeeCard = ({ name, jobTitle, department}) => {
+
+    return(
+        <div className="employee-card">
+                    <img src="https://www.achievers.com/wp-content/uploads/2020/05/05-27-20-2.jpg" className="employee-icon" />
+                    <div className="card-label">
+                      Name: <span className="card-value">{name}</span>
+                    </div>
+                    <div className="card-label">
+                      Job Title: <span className="card-value">{jobTitle}</span>
+                    </div>
+                    <div className="card-label">
+                      Department: <span className="card-value">{department}</span>
+                    </div>
+                    <button 
+                        className="card-button" 
+                        onClick={()=> console.log('You clicked on' + name  + department)}
+                    >
+                        See Profile
+                    </button>
+        </div>
+    )
+
+}
+
+
+export { EmployeeCard }
